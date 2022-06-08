d5a571640f2d7322f0ca006a9c8b9d50331f2a2c
ttronge <tomastronge@gmail.com>
2022-06-07 21:50:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let name = 'tomi';
+let age = 22;
+let precio = 250;
+let series = ['Dr House', 'Friends', 'Bojack horseman'];
+
+let films = [
+    titanic = {
+        name: 'titanic',
+        year: 1998,
+        protagonists: ['rose', 'Jack', 'pepe'],
+    },
+    backToFuture = {
+        name: 'back to the future',
+        year: 1980,
+        protagonists: ['Dr Brown', 'Martin', 'Biff'],
+    }
+];
+// ejercicio 1
+//console.log(name,age,precio,seres,films);
+
+//ejercicio 2
+age ++
+console.log(age);
+
+// ejercicio 3 
+series.push('Futurama')
+console.log(seres);
